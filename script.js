9039747525d9028de085487258a637d8cc89c01e
swawiye <sovereign.wawiye@gmail.com>
2025-03-25 22:53:52 +0300

    formatting & responsiveness

---
+++ script.js
@@ -0,0 +1,11 @@
+function onSubmit() {
+    alert("Welcome back :)")
+    return true;
+};
+
+function welcomeMsg(name) {
+    name = "Kelsey"
+    alert("Hello" + " " + name + "!" + " " + "Welcome to our website!")
+};
+
+
